5b6e9ddfdeb4528a213c24a8b578105feba5f29f
Freshizy <mohgli93@gmail.com>
2024-09-27 23:57:17 -0400

    defined several functions to generate and display results. game 1 is completed

--- Rock Paper Scissors/rps.js
+++ Rock Paper Scissors/rps.js
@@ -10,22 +10,28 @@ const resultDisplay = document.getElementById('result')
 //when the users clicks the button, display the value of the button id from html page. 
 
 const possibleChoices = document.querySelectorAll('button')
+
 let userChoice
 let computerChoice
+let result 
+
 possibleChoices.forEach(choice => choice.addEventListener('click', (e) => {
     userChoice = e.target.id
     userChoiceDisplay.innerHTML = userChoice
     // when user clicks on either option the game will also generate the computers choice
     generateComputerChoice()
+    getResult()
 }))   
 
 // here is the function defined for the computer's choice.
 function generateComputerChoice() {
-    
+
     /* here we can generate a random number from 0-1 * 3(<-- number of buttons)... for readability i added 1
      so than later when defining what each number represents its easier to work between 1 2 or 3 */
 
     const randomNumber = Math.floor(Math.random() * possibleChoices.length + 1)
+    // there is a quick way to write the code below 
+
 
     if (randomNumber === 1){
         computerChoice = 'Rock'
@@ -38,6 +44,33 @@ function generateComputerChoice() {
     }
     computerChoiceDisplay.innerHTML = computerChoice
 }
+
+// function below is the result of both ... who wins ? ( also there is a quicker way to write this code)
+function getResult (){
+    if(computerChoice === userChoice){
+        result = 'It is a Draw!';
+    }
+    if(computerChoice === 'Rock' && userChoice === 'Paper'){
+        result = 'You Win!';
+    }
+    if(computerChoice === 'Rock' && userChoice === 'Scissor'){
+        result = 'You Lost!';
+    }
+    if(computerChoice === 'Paper' && userChoice === 'Scissor'){
+        result = 'You Win!';
+    }
+    if(computerChoice === 'Paper' && userChoice === 'Rock'){
+        result = 'You Lose!';
+    }
+    if(computerChoice === 'Scissors' && userChoice === 'Rock'){
+        result = 'You Win!';
+    }
+    if(computerChoice === 'Rock' && userChoice === 'Scissors'){
+        result = 'You Lose!';
+    }
+    resultDisplay.innerHTML = result
+}
+
 
 
 
